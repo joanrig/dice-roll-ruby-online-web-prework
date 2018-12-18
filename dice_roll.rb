348ac5fa3dca94873e2e9fdeb02abec22ccda4fe
Joan Lyness <joanrigdon@gmail.com>
2018-12-18 15:00:15 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -4,8 +4,8 @@
 # Feel free to google "how to generate a random number in ruby"
 srand
 
-def random_select()
+def random_select(array, n)
   array =  [1,2,3,4,5,6]
-  puts [1,2,3,4,5,6].sample
+  n = 2
 end
 
